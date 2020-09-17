6f9ab850b3839b9a6c2a35efd020655a6f1f32ae
denzelawuah <dawuah17@gmail.com>
2020-09-17 16:43:50 -0400

    get top scorers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
   title = 'SoccerSite';
+  constructor(){
+      //Top scorcers section
+
+    }
+
+
+  getScorers(){
+    fetch("https://api-football-v1.p.rapidapi.com/v2/topscorers/4", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    "x-rapidapi-key": "01217d9c47msha27496e5b78af28p1bca54jsnb9d860de87bc",
+    'Content-Type': 'application/json'
+	}
+})
+.then(response => {
+	console.log(response.json());
+})
+.catch(err => {
+	console.log(err);
+});
+
+  }
+
+  
+
+
+
 }
